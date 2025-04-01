a9cb97df6ac01a7ce1432606d009a5be124df8af
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2025-04-01 22:45:41 +0400

    Update CERTIFICATES.jsx

--- src/Component/CERTIFICATES/CERTIFICATES.jsx
+++ src/Component/CERTIFICATES/CERTIFICATES.jsx
@@ -6,7 +6,7 @@ import CERTIFICATE3 from "../../IMG/CERTIFICATE3.png"
 import "./CERTIFICATES.css";
 
 
-const Certificates = () => {
+const CERTIFICATES = () => {
   return (
     <section className='CERTIFICATES'>
         <Container>
@@ -27,4 +27,4 @@ const Certificates = () => {
   )
 }
 
-export default Certificates
+export default CERTIFICATES
