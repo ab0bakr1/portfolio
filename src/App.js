d0d0b3a575b2e6fe3c4421ad11fb7a208588fce8
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2025-04-02 06:39:15 +0400

    Upd App.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from './Component/Skills/Skills';
 import Portfolio from './Component/Portfolio/Portfolio';
 import Contact from './Component/Contact/Contact';
 import Footer from './Component/Footer/Footer';
-import Certificates from './Component/CERTIFICATES/CERTIFICATES ';
+import CERTIFICATES from './Component/CERTIFICATES/CERTIFICATES ';
 AOS.init();
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
       <Hero />
       <Skills />
       <Portfolio />
-      <Certificates />
+      <CERTIFICATES />
       <Contact />
       <Footer />
     </>
