4125602a8a1a8bd6dea09a0f0059e1c1b7736c48
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2025-08-15 19:45:32 +0300

    Update Header.jsx

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -13,7 +13,7 @@ const variants = {
       },
     },
     closed: {
-      clipPath: "clip-path: circle(30px at calc(100% - 15%) 35px);",
+      clipPath: "circle(30px at calc(100% - 15%) 35px);",
       transition: {
         delay: 0.5,
         type: "spring",
